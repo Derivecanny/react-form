8c64aeab537259f879f0fca93d112e17c21e0826
Derivecanny <gurugi006@gmail.com>
2021-11-26 14:02:54 +0530

    add react-router-dom

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -27,7 +27,7 @@ function signup() {
 
         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
         <p className="forgot-password text-right">
-            Already registered <a href="#">sign in?</a>
+            Already registered <a href="/Login">sign in?</a>
         </p>
     </form>
     )
